15e3a63de00f3ea0f7d739b218873d9a7b85c088
Avraam Lefterov <avraamlefterov@yandex.ru>
2025-09-03 18:07:14 +0300

    Add files via upload

---
+++ HomeWork1Main.java
@@ -0,0 +1,14 @@
+package org.example.Netology.HomeWork1;
+
+public class HomeWork1Main {
+    public static void main(String[] args) {
+        String name = "Avraam Lefterov"; // задание №1
+        System.out.println(name);
+
+        int income = 57000; // задание #2
+        int spending = 24571;
+        int accountBalance = income - spending;
+        System.out.println("Итого (руб): " + "\n" + accountBalance);
+
+    }
+}
