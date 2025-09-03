4a20a8713a6d351730a35bd3f8136c71132b5591
Avraam Lefterov <avraamlefterov@yandex.ru>
2025-09-03 17:58:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Avraam Lefterov"; // задание №1
+        System.out.println(name);
+
+        int income = 57000; // задание #2
+        int spending = 24571;
+        int accountBalance = income - spending;
+        System.out.println(accountBalance);
+    }
+}
